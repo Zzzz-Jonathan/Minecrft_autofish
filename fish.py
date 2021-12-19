0d744d077d78737d8f5af462b19a171a527f4b75
Jonathan Zhang <78021210+Zzzz-Jonathan@users.noreply.github.com>
2021-12-19 15:46:13 +0800

    Initial commit

---
+++ fish.py
@@ -0,0 +1,79 @@
+# -*- coding: UTF-8 -*-
+import pyautogui
+import time
+from cnocr import CnOcr
+import cv2
+from PIL import Image
+import numpy as np
+
+pyautogui.PAUSE = 1
+
+
+class Fish:
+    def findfish(self, res):
+        for line in res:
+            if line[0] == ['浮', '漂', '：', '溅', '起', '水', '花']:
+                return True
+
+        return False
+
+    def fish(self, img):
+        ocr = CnOcr()
+        # img = pyautogui.screenshot(region=(1455, 800, 200, 200))
+        # img = np.asarray(img)
+
+        textImg = self.detect(img)
+        ii = Image.fromarray(textImg.astype('uint8')).convert('RGB')
+        ii.save(str(time.time())+'.jpg')
+
+        # cnocr识别文本
+        res = ocr.ocr(textImg)
+        print("Predicted Chars:", res)
+
+        if self.findfish(res):
+            pyautogui.click(button='right')
+            pyautogui.click(button='right')
+            time.sleep(1)
+        else:
+            time.sleep(0.5)
+
+    def detect(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        dilation = self.preprocess(gray)
+        x, y, w, h = self.findTextRegion(dilation)
+
+        return img[y:y + h, x:x + w]
+
+    def preprocess(self, gray):
+        # Sobel算子
+        sobel = cv2.Sobel(gray, cv2.CV_8U, 1, 0, ksize=3)
+        _, binary = cv2.threshold(sobel, 0, 255, cv2.THRESH_OTSU + cv2.THRESH_BINARY)
+        element1 = cv2.getStructuringElement(cv2.MORPH_RECT, (30, 9))
+        element2 = cv2.getStructuringElement(cv2.MORPH_RECT, (24, 6))
+        dilation = cv2.dilate(binary, element2, iterations=1)
+        erosion = cv2.erode(dilation, element1, iterations=1)
+        dilation2 = cv2.dilate(erosion, element2, iterations=2)
+
+        return dilation2
+
+    def findTextRegion(self, img):
+        # 查找轮廓
+        contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        maxArea = 0
+        maxContour = 0
+        if len(contours) == 0:
+            return 0, 0, 0, 0
+        for i in range(len(contours)):
+            cnt = contours[i]
+            area = cv2.contourArea(cnt)
+            if area > maxArea:
+                maxArea = area
+                maxContour = cnt
+        x, y, w, h = cv2.boundingRect(maxContour)
+
+        return x, y, w, h
+
+# fish = Fish()
+# time.sleep(5)
+# while True:
+#     fish.fish(None)
---
+++ main.py
@@ -0,0 +1,75 @@
+from PIL import ImageGrab
+from torchvision import transforms
+from models import *
+from utils.utils import *
+from fish import Fish
+import time
+
+SIZE = 512
+SCREEN_W = 0
+SCREEN_H = 0
+CHECK_TIME = 0
+
+
+class TextPos:
+    def __init__(self):
+        self.loader = transforms.Compose([transforms.ToTensor()])
+        self.model = Darknet("yolov3-custom.cfg", img_size=SIZE).to('cpu')
+        self.model.load_state_dict(torch.load('weights.pth', map_location='cpu'))
+        self.model.eval()
+
+    def check_position(self):
+        tensor = self.loader(grab(SIZE))
+        tensor = tensor.unsqueeze(0)
+        predict = non_max_suppression(self.model(tensor), 0.8, 0.1)
+        if predict[0] is not None:
+            predict = list(predict[0][0])
+
+            # w = 2 * (predict[2] - predict[0])
+            # h = 2 * (predict[3] - predict[1])
+            # x = (predict[0] - 0.25 * w) * SCREEN_W / SIZE
+            # y = (predict[1] - 0.25 * h) * SCREEN_H / SIZE
+            #
+            # if x + w >= SCREEN_W:
+            #     w = SCREEN_W - x - 10
+            # if y + h > SCREEN_H:
+            #     h = SCREEN_H - y - 10
+            x1, y1 = int(predict[0]) * SCREEN_W / SIZE, int(predict[1]) * SCREEN_H / SIZE
+            x2, y2 = int(predict[2]) * SCREEN_W / SIZE, int(predict[3]) * SCREEN_H / SIZE
+            w = x2 - x1
+            h = max(y2 - y1, 150)
+
+            return max(x1-w, 0), max(y1-h, 0), min(x2+w, SCREEN_W), min(y2+h, SCREEN_H)
+        return 0, 0, 0, 0
+
+
+def grab(size=None):  # works on macOS(RGBA) and Windows(RGB) only
+    im = ImageGrab.grab()  # What region to copy. Default is the entire screen
+    if size is not None:
+        im = im.resize((size, size))
+
+    return im.convert('RGB')
+
+
+if __name__ == '__main__':
+    SCREEN_W = grab().width
+    SCREEN_H = grab().height
+
+    pos = TextPos()
+    fish = Fish()
+
+    px1, py1, px2, py2 = 0, 0, 0, 0
+    while (px1, py1, px2, py2) == (0, 0, 0, 0):
+        px1, py1, px2, py2 = pos.check_position()
+        time.sleep(5)
+    CHECK_TIME = time.time()
+
+    while True:
+        if CHECK_TIME - time.time() > 300:
+            px1, py1, px2, py2 = pos.check_position()
+            CHECK_TIME = time.time()
+
+        im = grab().crop((px1, py1, px2, py2))
+        im.save('./img/'+str(time.time())+'.jpg')
+        img = np.array(im)
+        fish.fish(img)
